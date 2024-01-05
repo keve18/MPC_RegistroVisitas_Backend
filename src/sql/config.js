95c886a37ab6e80d86e4449b61a50dcd160dc218
keve18 <keve18@live.com>
2024-01-05 12:57:40 -0500

    cambiamos variable de desarrollo en config.js

--- src/sql/config.js
+++ src/sql/config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const bd = mysql.createPool({
     host: process.env.HOST || process.env.HOSTD,
     user: process.env.USER || process.env.USERD,
-    password: process.env.PASS || process.env.PASS,
+    password: process.env.PASS || process.env.PASSD,
     database: process.env.BD || process.env.BDD,
     connectionLimit: 10, // Establecer el límite de conexiones
     waitForConnections: true, // Permitir esperar si se alcanza el límite
